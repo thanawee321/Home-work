4235fb74f5f36a126dc9ff19f1aa0313ea451868
thanawee321 <53590032+thanawee321@users.noreply.github.com>
2020-07-19 18:51:55 +0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTestSkill
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            textBox4.IsEnabled = false;
+        }
+
+        private void cal(double income,double expense,double itemwant) {
+
+            double keepMoney = (income - expense);
+            double alldate = itemwant / keepMoney;
+            textBox4.Text = alldate.ToString();
+
+
+                    
+
+        
+
+        
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double inCome = double.Parse(textBox1.Text);
+                double exPense = double.Parse(textBox2.Text);
+                double itemWant = double.Parse(textBox3.Text);
+
+                cal(inCome, exPense, itemWant);
+            }
+            catch (Exception ex) {
+
+                MessageBox.Show("ERROR"+ex.Message);
+            }
+
+        }
+
+
+    }
+}
